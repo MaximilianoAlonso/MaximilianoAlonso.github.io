8f09a170a36a8cccabba541c19b06f62be26d6b9
Maximiliano Alonso <84605497+MaximilianoAlonso@users.noreply.github.com>
2023-01-27 18:07:37 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+const btnmenu = document.querySelector("#iconomenu");
+
+const menu = document.querySelector("#mostrar");
+
+btnmenu.addEventListener("click", function () {
+    menu.classList.toggle("mostrarmas")
+})
